58f0a7990102f3662d836f432bc815c13b2bfb6c
Abdul Basit <abdulbasit10397@gmail.com>
2023-11-30 21:52:29 +0500

    PS-10 Optimized solution for reverse vowel string

---
+++ src/problem5/ReverseStringVowels_Optimized.java
@@ -0,0 +1,38 @@
+package problem5;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ReverseStringVowels_Optimized {
+
+    public static void main(String[] args) {
+        System.out.println(reverseVowels("hello"));
+    }
+
+    public static String reverseVowels(String s) {
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+        char[] result = s.toCharArray();
+
+        int start = 0, end = s.length() - 1;
+        while (start <= end) {
+
+            // Move start pointer until it points to a vowel
+            while (start < end && !vowels.contains(s.charAt(start))) start++;
+
+            // Move end pointer until it points to a vowel
+            while (start < end && !vowels.contains(s.charAt(end))) end--;
+
+            // Swap the vowels
+            char temp = result[start];
+            result[start] = result[end];
+            result[end] = temp;
+
+            // Move both the pointers towards each other
+            start++;    end--;
+        }
+
+        return new String(result);
+    }
+
+}
