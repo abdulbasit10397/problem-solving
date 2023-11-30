522f495575f3f392b2dc3de7b9d68a7b431a17f8
Abdul Basit <abdulbasit10397@gmail.com>
2023-11-30 20:00:25 +0500

    PS-09 Optimized solution for can place flowers problem

--- src/problem4/CanPlaceFlowers_Optimized.java
+++ src/problem4/CanPlaceFlowers_Optimized.java
@@ -1,5 +1,7 @@
 package problem4;
 
+import java.util.Stack;
+
 /**
  * Optimized Solution for can place flowers problem
  */
@@ -11,11 +13,24 @@ public class CanPlaceFlowers_Optimized {
 
 
     public static boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int flowers = 0;
+        int flowersThatCanBePlanted = 0;
         for (int i = 0; i < flowerbed.length; i++) {
+            // Check if the current plot is empty.
+            if (flowerbed[i] == 0) {
+                // Check if the left and right plots are empty.
+                boolean emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
+                boolean emptyRightPlot = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
 
+                // If both plots are empty, we can plant a flower here.
+                if (emptyLeftPlot && emptyRightPlot) {
+                    flowerbed[i] = 1;
+                    flowersThatCanBePlanted++;
+                    if (flowersThatCanBePlanted >= n) {
+                        return true;
+                    }
+                }
+            }
         }
-
-        return flowers >= n;
+        return flowersThatCanBePlanted >= n;
     }
 }
