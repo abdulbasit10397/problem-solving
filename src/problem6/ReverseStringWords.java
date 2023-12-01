93a7d8c05ac96f72a21127f112ae7fc6d3fadb00
Abdul Basit <abdulbasit10397@gmail.com>
2023-12-01 20:21:13 +0500

    PS-12 Reverse Words in a String Solution

---
+++ src/problem6/ReverseStringWords.java
@@ -0,0 +1,24 @@
+package problem6;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ReverseStringWords {
+
+    public static void main(String[] args) {
+        System.out.println(reverseWords("  hello world  "));
+    }
+
+    public static String reverseWords(String s) {
+        String[] parts = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = parts.length - 1; i >= 0; i--) {
+            if(parts[i].trim().equals("")) continue;
+            sb.append(parts[i]);
+            if(i>0) sb.append(" ");
+        }
+
+        return sb.toString().trim();
+    }
+}
