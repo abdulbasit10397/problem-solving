2a1d05d7ccee4b4a1a06aa120787c734460d9cc2
Abdul Basit <abdulbasit10397@gmail.com>
2023-12-10 17:39:50 +0500

    PS-09 Unique no of occurrences solution

--- src/problem9/UniqueOccurrences.java
+++ src/problem9/UniqueOccurrences.java
@@ -1,7 +1,37 @@
 package problem9;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class UniqueOccurrences {
     public static void main (String[] args) {
+        int[] arr = {-3,0,1,-3,1,1,1,-3,10,0};
 
+        System.out.println(uniqueOccurrences(arr));
+    }
+
+    public static boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> numberOccurrenceMap = new HashMap<>();
+        Set<Integer> occurrenceSet = new HashSet<>();
+
+        for(int n : arr) {
+            if(numberOccurrenceMap.containsKey(n)) {
+                int occurrence = numberOccurrenceMap.get(n);
+                numberOccurrenceMap.put(n, ++occurrence);
+                continue;
+            }
+
+            numberOccurrenceMap.put(n, 1);
+        }
+
+        for(Map.Entry<Integer, Integer> entry : numberOccurrenceMap.entrySet()) {
+            if(occurrenceSet.contains(entry.getValue())) return false;
+
+            occurrenceSet.add(entry.getValue());
+        }
+
+        return true;
     }
 }
